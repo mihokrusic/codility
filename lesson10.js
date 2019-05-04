09aec2977664a6de1b5e8e6168ee73fe646e5f65
mkrusic <mkrusic@neogov.net>
2019-05-04 09:27:11 +0200

    Lesson 10, task 2

--- lesson10.js
+++ lesson10.js
@@ -38,37 +38,107 @@ function minPerimiterRectangle(N) {
 function peaks(A) {
     A = [1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2];
 
-    let peaks = {};
+    let peaks = [];
 
-    for (let i = 1; i < A.length - 1; i++) {
+    // Get peaks
+    for (let i = 1; i < A.length - 1; i++)
         if (A[i] > A[i - 1] && A[i] > A[i + 1])
-            peaks[i] = true;
-    }
+            peaks.push(i);
 
+    if (peaks.length === 0)
+        return 0;
+
+    // Get blocks
     let blocks = [];
-    let i = 1;
-    let maxBlocks = Math.floor(A.length / 3);
+    let i = 2;
     while (i * i <= A.length) {
         if (A.length % i === 0) {
             blocks.push(i);
-            if (i * i !== A.length && A.length / i < maxBlocks)
+            if (i * i !== A.length)
                 blocks.push(A.length / i);
         }
-
         i++;
     }
     blocks.sort((a, b) => a > b);
 
-    console.log(peaks);
-    console.log(blocks);
+    // Check blocks
+    let maxPossibleBlock = 1;
+    for (let i = 0; i < blocks.length; i++) {
+        let blockIsOk = true;
+        let block = blocks[i];
+        let blockSize = A.length / block;
+        for (let b = 0; b < block; b++) {
+            let blockSectionStart = blockSize * b;
+            let blockSectionEnd = blockSize * (b + 1) - 1;
+            let blockSectionHasPeak = false;
+            for (let k = 0; k < peaks.length; k++) {
+                if (peaks[k] < blockSectionStart)
+                    continue;
 
-    return 12;
+                if (peaks[k] >= blockSectionStart && peaks[k] <= blockSectionEnd) {
+                    blockSectionHasPeak = true;
+                    break;
+                }
+
+                if (peaks[k] > blockSectionEnd)
+                    break;
+            }
+            if (!blockSectionHasPeak) {
+                blockIsOk = false;
+                break;
+            }
+        }
+
+        if (blockIsOk)
+            maxPossibleBlock = block;
+    }
+
+    return maxPossibleBlock;
+}
+
+function flags(A) {
+    A = [1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2];
+
+    let peaks = [];
+
+    // Get peaks
+    for (let i = 1; i < A.length - 1; i++)
+        if (A[i] > A[i - 1] && A[i] > A[i + 1])
+            peaks.push(i);
+
+    if (peaks.length === 0)
+        return 0;
+
+    let maxFlags = 1;
+    for (let i = 2; i <= peaks.length; i++) {
+        let currentFlagCount = 1;
+        let lastFlagPeak = peaks[0];
+        let flagsCanBePlaced = false;
+        for (let j = 1; j < peaks.length; j++) {
+
+            if (peaks[j] - lastFlagPeak >= i) {
+                currentFlagCount++;
+                lastFlagPeak = peaks[j];
+            }
+
+            if (currentFlagCount === i)
+                flagsCanBePlaced = true;
+        }
+
+        if (flagsCanBePlaced)
+            maxFlags = i;
+        else
+            break;
+    }
+
+    return maxFlags;
 }
 
 module.exports = {
     1: countFactors,
     2: minPerimiterRectangle,
     3: peaks,
+    4: flags
 };
 
 
